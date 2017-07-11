714a976b772c82cdfe74a7a538693b1727fb7589
devinASC4 <devincastillo57@gmail.com>
2017-07-11 08:59:59 -0700

    first commit

---
+++ pokemon.array.js
@@ -0,0 +1,16 @@
+//var myPokemonRoster = ["lucario","blaziken","mewtwo"];
+//var firstToFight = myArray[2];
+//var secondToFight = myArray[1];
+//var thirdToFight = myArray[0]; 
+//for (var i=0; i<10; i++) {
+
+//console.log(i);
+//}
+
+
+//var myArray = ["bob","mahdi","cyril","juan","mike"];
+//for (var i=0; i<4; i++){console.log(i)}
+var myPokemonRoster = ["lucario","blaziken","mewtwo"];
+for (var i=0; i<3; i++){
+    console.log(myPokemonRoster[i]);
+}
---
+++ pokemonVarriable.js
@@ -0,0 +1,9 @@
+var pokemonHP = 44; // number
+var squirtleNUM = 7; // number
+var squirtleType = "water"; // string
+var squirtleBestMove = "splash" // strong
+var squirtleLastEvo = false; // boolean
+var squirtleBodySize = 1;
+var squirtleRecruitRate = 11.7;
+
+
