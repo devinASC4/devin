d00d0e52bf3a83ae6f1c1790e4744d489100e60e
devinASC4 <devincastillo57@gmail.com>
2017-07-12 08:09:35 -0700

    second command

---
+++ welloHorld.js
@@ -0,0 +1,18 @@
+//function wellohorld(){
+ //   return "Wello Horld!";
+  //  console.log("Wello Horld!");
+//}
+
+function rand(num){
+    // returns a random number between zero and num
+    return Math.floor(Math.random()*num);
+}
+//var randNum = Math.random()*num;
+//var result = Math.floor(randNum);// floor rounds down 
+//return result;
+
+
+
+console.log(rand(4));
+
+
